f06ee3bc42e487915f374b79dd97a9da556024a3
juliojlgon <juliojlgon@gmail.com>
2016-02-13 21:53:49 +0100

    Initial-Setup

---
+++ Bicimad.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bicimad.Web.Startup))]
+namespace Bicimad.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
