c103f4a1101007b48fcb2256e6b9509e32327acc
juliojlgon <juliojlgon@gmail.com>
2016-02-16 14:52:39 +0100

    DateTimeHelper

---
+++ ClassLibrary1/DateTimeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bicimad.Helpers
+{
+    public static class DateTimeHelper
+    {
+        private const string SPAIN_ZONE_ID = "Romance Standard Time";
+
+        public static DateTime SpanishNow
+        {
+            get
+            {
+                var cstZone = TimeZoneInfo.FindSystemTimeZoneById(SPAIN_ZONE_ID);
+                var cstTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cstZone);
+                return cstTime;
+            }
+        }
+
+        public static string ToDateFormat(DateTime date)
+        {
+            return date.ToShortDateString();
+        }
+
+        public static string ToDateTimeFormat(DateTime date)
+        {
+            return ToDateFormat(date) + " " + date.ToShortTimeString();
+        }
+    }
+}
