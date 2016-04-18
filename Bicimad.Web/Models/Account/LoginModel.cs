a3da602534b0f69119e20ab33816df8c8cf805b7
Mgomezp <manuel.gom.p@gmail.com>
2016-04-18 09:50:02 +0200

    UI translated to english.

--- Bicimad.Web/Models/Account/LoginModel.cs
+++ Bicimad.Web/Models/Account/LoginModel.cs
@@ -4,13 +4,13 @@ namespace Bicimad.Web.Models.AccountModels
 {
     public class LoginModel
     {
-        [Required(ErrorMessage = "Obligatorio")]
-        [Display(Name = "Nombre de usuario o email")]
+        [Required(ErrorMessage = "Field can't be empty.")]
+        [Display(Name = "Username or email")]
         public string UserNameOrEmail { get; set; }
 
-        [Required(ErrorMessage = "Obligatorio")]
+        [Required(ErrorMessage = "Field can't be empty.")]
         [DataType(DataType.Password)]
-        [Display(Name = "Contraseña")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
--- Bicimad.Web/Models/Account/RegisterModel.cs
+++ Bicimad.Web/Models/Account/RegisterModel.cs
@@ -9,27 +9,27 @@ namespace Bicimad.Web.Models.AccountModels
 {
     public class RegisterModel
     {
-        [Required(ErrorMessage = "Obligatorio")]
-        [Remote("ValidateEmailUnique", "Account", ErrorMessage = "Ese email ya existe")]
+        [Required(ErrorMessage = "Enter a valid email")]
+        [Remote("ValidateEmailUnique", "Account", ErrorMessage = "This email is already in use")]
         [MaxLength(128)]
-        [EmailAddress(ErrorMessage = "Email incorrecto")]
+        [EmailAddress(ErrorMessage = "Wrong email")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Obligatorio")]
-        [Display(Name = "Nombre de usuario")]
-        [Remote("ValidateUserNameUnique", "Account", ErrorMessage = "Ese nombre de usuario ya existe")]
+        [Required(ErrorMessage = "Enter your firstname")]
+        [Display(Name = "Username")]
+        [Remote("ValidateUserNameUnique", "Account", ErrorMessage = "This Username is already in use")]
         [MaxLength(64)]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Obligatorio")]
-        [StringLength(100, ErrorMessage = "Mínimo {2} caracteres", MinimumLength = 6)]
+        [Required(ErrorMessage = "Mandatory")]
+        [StringLength(100, ErrorMessage = "Password must be at least {2} Characters", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Contraseña")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirmar Contraseña")]
-        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        [Display(Name = "Confirm your password:")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password do not match")]
         public string ConfirmPassword { get; set; }
 
     }
