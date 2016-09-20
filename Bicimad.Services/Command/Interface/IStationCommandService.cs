de5d789948809a6fe084f2cd08755c88fa4dfbbb
juliojlgon <juliojlgon@gmail.com>
2016-09-21 01:30:33 +0200

    IStationCommandService updated

--- Bicimad.Services/Command/Interface/IStationCommandService.cs
+++ Bicimad.Services/Command/Interface/IStationCommandService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bicimad.Enums;
 using Bicimad.Services.Command.Commands;
 
 namespace Bicimad.Services.Command.Interface
 {
     public interface IStationCommandService
     {
+        CommandResult SetDiscountType(List<string> stationIds, DiscountType discountType);
+        CommandResult SetDiscount(List<string> stationIds, double discount);
     }
 }
