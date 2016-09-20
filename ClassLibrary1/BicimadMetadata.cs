627ba99ed4623f32292b8d4f50099b0a439d55a1
juliojlgon <juliojlgon@gmail.com>
2016-09-21 00:08:53 +0200

    Added Metadata for saving the BasePrice

---
+++ ClassLibrary1/BicimadMetadata.cs
@@ -0,0 +1,18 @@
+﻿namespace Bicimad.Helpers
+{
+    public static class BicimadMetadata
+    {
+        private static int _basePrice = 5;
+
+        public static int BicimadBasePrice
+        {
+            get { return BasePrice; }
+        }
+
+        public static int BasePrice
+        {
+            get { return _basePrice; }
+            set { _basePrice = value; }
+        }
+    }
+}
