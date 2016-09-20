7b21488e291560c91cf0533d54803f3d1851d9b7
juliojlgon <juliojlgon@gmail.com>
2016-09-21 00:33:38 +0200

    Discount added to stations.

---
+++ Bicimad.Enums/DiscountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicimad.Enums
+{
+    public enum DiscountType
+    {
+        Constant = 0,
+        Porcentual = 1
+    }
+}
