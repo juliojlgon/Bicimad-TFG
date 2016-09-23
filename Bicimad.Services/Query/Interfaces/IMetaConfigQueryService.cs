6c9defa55734318298d7542ad910e0c6d9d58340
juliojlgon <juliojlgon@gmail.com>
2016-09-23 13:47:28 +0200

    Bug with metadata solved. MetaConfig added to DB.

---
+++ Bicimad.Services/Query/Interfaces/IMetaConfigQueryService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Bicimad.Enums;
+using Bicimad.Services.Query.Dto.MetaConfig;
+
+namespace Bicimad.Services.Query.Interfaces
+{
+    public interface IMetaConfigQueryService
+    {
+        List<MetaConfigDto> Get(MetaConfigKey key);
+    }
+}
